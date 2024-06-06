59573738ad71d2e1b464879a18a1ef7fdc870156
Lenoridev <lenori233@gmail.com>
2024-06-06 21:51:18 +0200

    Initial commit

---
+++ src/main/java/net/lenoridev/ClashOfTanks.java
@@ -0,0 +1,11 @@
+package net.lenoridev;
+
+import net.lenoridev.ui.Window;
+
+public class ClashOfTanks {
+
+    public static void main(String[] args) {
+        Window window = Window.get();
+        window.run();
+    }
+}
